92f62fb4cbc0dd2c8eb6f26f62d5c3ceaeff7798
Skanda2007 <82853099+Skanda2007@users.noreply.github.com>
2021-08-13 13:09:13 +0530

    Improved the file

--- findthesquare.c
+++ findthesquare.c
@@ -5,11 +5,24 @@ int main()
 {
     int num;
     int number=1;
+    int check;
     printf("Enter a number >>");
     scanf("%d", &num);
+    check = num/2;
     while (num/number != number)
     {
         number++;
+        if(number > check)
+        {
+            break;
+        }
     }
-    printf("The square of %d is %d", num, number);
+    if(number*number == num)
+    {
+        printf("The square of %d is %d", num, number);
+    }
+    else
+    {
+        printf("%d is not a perfect square\n", num);
+    }
 }
